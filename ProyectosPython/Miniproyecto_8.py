f6f848bb9ea5ec748d97059da17fb84c7346c063
netomixs <deleon.gallegos.20056@itsmante.edu.mx>
2024-07-01 11:25:42 -0600

    Proyectos Python

--- ProyectosPython/Miniproyecto_8.py
+++ ProyectosPython/Miniproyecto_8.py
@@ -3,39 +3,26 @@ import tkinter as tk
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
+from tkinter import messagebox
 codes = USSDCodes()
 print(codes.get())
+
+def mostrarCodigo():
+    code = entrada_letra.get()
+    des=codes.get().get(code,"Codigo no encontrado")
+    if des:
+        messagebox.showinfo("Codigo",des)
+
 ventana = tk.Tk()
 ventana.title("Codigos USSD")
-ventana.geometry("600x1200") 
-canvas = tk.Canvas(ventana)
-scrollbar = ttk.Scrollbar(ventana, orient="vertical", command=canvas.yview)
-scrollable_frame = ttk.Frame(canvas)
-scrollable_frame.bind(
-    "<Configure>",
-    lambda e: canvas.configure(
-        scrollregion=canvas.bbox("all")
-    )
-)
-canvas.create_window((200, 0), window=scrollable_frame, anchor="nw")
-canvas.configure(yscrollcommand=scrollbar.set)
-for item in codes.get():
-    frame = ttk.Frame(scrollable_frame)
-    frame.pack(fill="x", pady=5)
+ventana.geometry("600x600") 
 
-    code_frame = ttk.Frame(frame)
-    code_frame.pack(side="left", fill="y", padx=5)
-    label_Code = tk.Label(code_frame, text=item['codigo'], font=('Arial', 14, 'bold'))
-    label_Code.pack(anchor="w")
+etiqueta_palabra = tk.Label(ventana, text="Ingresa codigo", font=('Helvetica', 20))
+etiqueta_palabra.pack(pady=10)
 
-    info_frame = ttk.Frame(frame)
-    info_frame.pack(side="left", fill="both", padx=5, expand=True)
-    label_nombre = tk.Label(info_frame, text=item["Descripcion"], font=('Arial', 11))
-    label_nombre.pack(anchor="w", padx=(10, 0))  
+entrada_letra = tk.Entry(ventana,width=50)
+entrada_letra.pack(pady=10)
 
-
-# Empaquetar el canvas y el scrollbar en la ventana
-canvas.pack(side="left", fill="both", expand=True)
-scrollbar.pack(side="right", fill="y")
-
+boton_intentar = tk.Button(ventana, text="Enviar", command=mostrarCodigo,width=25)
+boton_intentar.pack(pady=10)
 ventana.mainloop()
--- ProyectosPython/USSDCodes.py
+++ ProyectosPython/USSDCodes.py
@@ -1,40 +1,31 @@
 
 class USSDCodes:
     def __init__(self):
-        self.data = [
-            {"codigo": "##4636##", "Descripcion": "Mostrar información sobre el teléfono, la batería y las estadísticas de uso"},
-            {"codigo": "##7780##",
-                "Descripcion": "Restablecer el teléfono a estado de fábrica (elimina datos y aplicaciones)"},
-            {"codigo": "27673855#",
-                "Descripcion": "Borrado completo del móvil y reinstalación del firmware"},
-            {"codigo": "##34971539##",
-                "Descripcion": "Mostrar información completa sobre la cámara"},
-            {"codigo": "##7594##",
-                "Descripcion": "Cambiar el comportamiento del botón de encendido (apagado directo al habilitar el código)"},
-            {"codigo": "##273283255663282*##",
-                "Descripcion": "Copia de seguridad rápida de todos tus archivos multimedia"},
-            {"codigo": "##197328640##",
-                "Descripcion": "Habilitar el modo de prueba para la actividad del servicio"},
-            {"codigo": "##232339##", "Descripcion": "Pruebas inalámbricas de LAN"},
-            {"codigo": "##526##", "Descripcion": "Pruebas inalámbricas de LAN"},
-            {"codigo": "##232338##",
-                "Descripcion": "Mostrar la dirección de Mac con Wi-Fi"},
-            {"codigo": "##1472365##", "Descripcion": "Prueba rápida de GPS"},
-            {"codigo": "##1575##", "Descripcion": "Prueba de GPS de tipo diferente"},
-            {"codigo": "##0283##", "Descripcion": "Prueba de bucle invertido"},
-            {"codigo": "##0*##", "Descripcion": "Prueba de pantalla LCD"},
-            {"codigo": "##0673##", "Descripcion": "Prueba de audio"},
-            {"codigo": "##0289##", "Descripcion": "Prueba de audio"},
-            {"codigo": "##0842##", "Descripcion": "Prueba de vibración y luz de fondo"},
-            {"codigo": "##2663##", "Descripcion": "Muestra la versión de pantalla táctil"},
-            {"codigo": "##2664##", "Descripcion": "Prueba de pantalla táctil"},
-            {"codigo": "##0588##", "Descripcion": "Prueba de sensor de proximidad"},
-            {"codigo": "##3264##", "Descripcion": "Versión RAM"},
-            {"codigo": "##232331##", "Descripcion": "Prueba de Bluetooth"},
-            {"codigo": "##7262626##", "Descripcion": "Prueba de campo"},
-            {"codigo": "##232337##",
-                "Descripcion": "Muestra la dirección del dispositivo Bluetooth"}
-        ]
-
+        self.data  = {
+    "##4636##": "Mostrar información sobre el teléfono, la batería y las estadísticas de uso",
+    "##7780##": "Restablecer el teléfono a estado de fábrica (elimina datos y aplicaciones)",
+    "27673855#": "Borrado completo del móvil y reinstalación del firmware",
+    "##34971539##": "Mostrar información completa sobre la cámara",
+    "##7594##": "Cambiar el comportamiento del botón de encendido (apagado directo al habilitar el código)",
+    "##273283255663282*##": "Copia de seguridad rápida de todos tus archivos multimedia",
+    "##197328640##": "Habilitar el modo de prueba para la actividad del servicio",
+    "##232339##": "Pruebas inalámbricas de LAN",
+    "##526##": "Pruebas inalámbricas de LAN",
+    "##232338##": "Mostrar la dirección de Mac con Wi-Fi",
+    "##1472365##": "Prueba rápida de GPS",
+    "##1575##": "Prueba de GPS de tipo diferente",
+    "##0283##": "Prueba de bucle invertido",
+    "##0*##": "Prueba de pantalla LCD",
+    "##0673##": "Prueba de audio",
+    "##0289##": "Prueba de audio",
+    "##0842##": "Prueba de vibración y luz de fondo",
+    "##2663##": "Muestra la versión de pantalla táctil",
+    "##2664##": "Prueba de pantalla táctil",
+    "##0588##": "Prueba de sensor de proximidad",
+    "##3264##": "Versión RAM",
+    "##232331##": "Prueba de Bluetooth",
+    "##7262626##": "Prueba de campo",
+    "##232337##": "Muestra la dirección del dispositivo Bluetooth"
+}
     def get(self):
         return self.data
